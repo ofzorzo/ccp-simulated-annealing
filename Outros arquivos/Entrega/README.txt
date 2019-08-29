d7044fbbbee7c41294afe0007a3d95ddb13c9a4f
Felipe Zorzo Pereira <felipe.1707@hotmail.com>
2019-08-29 11:11:14 -0300

    Files reorganized

--- Outros arquivos/Entrega/README.txt
+++
@@ -1,6 +0,0 @@
-Os parâmetros recebidos por simulatedAnnealing.py são descritos ao chamá-lo com o argumento -h.
-
-Ressalta-se que os dois primeiros são obrigatórios e posicionais. O primeiro deve ser o caminho para um arquivo em que o log da execução do algoritmo será salvo; o segundo deve ser o caminho para um arquivo que contenha uma instância do GBMV. Recomenda-se botar o caminho sempre entre aspas, como no exemplo abaixo.
-
-Exemplo de caminho para a criação de um arquivo de log:
-"D:/UFRGS/Quinto Semestre/Otimização Combinatória/Trabalho final/Outputs/log.txt"
--- Outros arquivos/callSA.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-cmd = ['python', "simulatedAnnealingNewRepresentation.py", "D:/UFRGS/Quinto Semestre/Otimização Combinatória/Trabalho final/instanciaTeste.ins", "-seed 0.6951820580649771", "-r 0.99", "-I 2500", "-pf 0.05", "-pi 0.85", "-out D:/UFRGS/Quinto Semestre/Otimização Combinatória/Trabalho final/Outputs/teste1.txt"]#, "D:/UFRGS/Quinto Semestre/Otimização Combinatória/Trabalho final/instanciaTeste.ins", "-r 0.99", "-I 2500", "-pf 0.05", "-pi 0.85", "-seed 0.6951820580649771", "-out D:/UFRGS/Quinto Semestre/Otimização Combinatória/Trabalho final/Outputs/teste1.txt"]
-process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-for line in process.stdout:
-	print(line)
